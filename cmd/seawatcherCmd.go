0f5f40a090689a5bd445b4e42f272efc48ff7538
Ben Lebherz <git@benleb.de>
2023-07-28 18:19:50 +0200

    add receiver for events from the manifold aws sns topic (todo: move to own module)

--- cmd/seawatcherCmd.go
+++ cmd/seawatcherCmd.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"net"
+
 	"github.com/benleb/gloomberg/internal/seawa"
+	"github.com/benleb/gloomberg/internal/web"
 	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -19,6 +22,11 @@ var seaWatcherCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(seaWatcherCmd)
 
+	// run manifold sns receiver endpoint
+	viper.SetDefault("manifoldSNS.enabled", false)
+	viper.SetDefault("manifoldSNS.host", net.IPv4(127, 0, 0, 1))
+	viper.SetDefault("manifoldSNS.port", viper.GetUint16("web.port")-1)
+
 	// grpc
 	seaWatcherCmd.Flags().Uint16("grpc-port", 31337, "gRPC server port")
 	_ = viper.BindPFlag("seawatcher.grpc.port", seaWatcherCmd.Flags().Lookup("grpc-port"))
@@ -30,7 +38,7 @@ func init() {
 	_ = viper.BindPFlag("seawatcher.grpc.client.host", seaWatcherCmd.Flags().Lookup("grpc"))
 }
 
-func runSeawatcher(cmd *cobra.Command, _ []string) {
+func runSeawatcher(_ *cobra.Command, _ []string) {
 	// find api key
 	var apiKey string
 	switch {
@@ -45,18 +53,24 @@ func runSeawatcher(cmd *cobra.Command, _ []string) {
 	// start sea watcher & loop forever
 	sw := seawa.NewSeaWatcher(apiKey, gb)
 
-	sw.Pr("⚓️ starting seawatcher")
+	sw.Pr("⚓️ seawatcher started... 🌊 👀")
 
 	if viper.GetBool("seawatcher.pubsub") {
 		go sw.SubscribeToPubsubMgmt()
 		sw.Pr("subscribed to mgmt channel…")
 
 		// publish a "SendSlugs" event to the management channel to request the slugs/events to subscribe to from the clients
-		sw.PublishSendSlugs()
+		go sw.PublishSendSlugs()
 		sw.Pr("requested slugs from clients…")
 	}
 
-	sw.Prf("seaWatcherCmd.CalledAs(): %+v", cmd.CalledAs())
+	// sw.Prf("seaWatcherCmd.CalledAs(): %+v", cmd.CalledAs())
+
+	//
+	// manifold SNS receiver
+	if viper.GetBool("manifoldSNS.enabled") {
+		go web.StartmanifoldSNS(gb)
+	}
 
 	select {}
 }
